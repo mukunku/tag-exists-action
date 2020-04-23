40aa8b60bff2d91002c128c9db2dff9fda93241c
mukunku <chunkymonkey_42@hotmail.com>
2020-04-23 18:10:30 -0400

    first try

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # tag-exists-action
 A Github action that determines if a tag exists
+
+## Inputs
+
+### `tag`
+
+**Required** The tag to search for.
+
+## Outputs
+
+### `exists`
+
+true or false
+
+## Example usage
+
+uses: actions/tag-exists@v1
+with:
+  tag: 'v1'
---
+++ index.js
@@ -0,0 +1,31 @@
+const core = require('@actions/core');
+const { context, GitHub } = require('@actions/github');
+
+try {
+    const tag = process.env.TAG || process.env.INPUT_TAG || '';
+
+    const github = new GitHub(process.env.GITHUB_TOKEN);
+
+    var exists = false;
+
+    try {
+        const getRefResponse = await github.git.getRef({
+            owner: context.owner,
+            repo: context.repo,
+            ref: `tags/${tag}`
+        });
+
+        const payload = JSON.stringify(getRefResponse);
+        console.log(`getRef payload: ${payload}`);
+
+        if (getRefResponse && getRefResponse.ref)
+            exists = true;
+
+    } catch(error) {
+        core.warning(`getRefResponse failed with error: ${error}`);
+    }
+
+    core.setOutput('exists', exists);
+} catch (error) {
+    core.setFailed(error.message);
+}
